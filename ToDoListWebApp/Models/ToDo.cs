5f637830c859474551b5bce1fe7074156565a4e4
chloe.minn.jia.ho <chloe.minn.jia.ho@WINAP4U4R5GNPPU>
2023-09-12 22:20:10 +0800

    Add project files.

---
+++ ToDoListWebApp/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoListWebApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+
+        public Status Status { get; set; } = Status.Created;
+
+    }
+}
